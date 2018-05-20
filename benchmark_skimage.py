ca9f6ea67385ff90628a79dae6a1ed629fecb052
Emmanuelle Gouillart <emmanuelle.gouillart@nsup.org>
2018-05-20 14:47:09 +0200

    First commit of benchmarking script

---
+++ benchmark_skimage.py
@@ -0,0 +1,72 @@
+import numpy as np
+import inspect
+from skimage import exposure, feature, filters, measure, morphology, \
+                    restoration, segmentation, transform, util, data, color
+from timeit import default_timer
+
+
+def only_one_nondefault(args):
+    """
+    Returns True if the function has only one non-keyword parameter,
+    False otherwise.
+    """
+    defaults = 0 if args.defaults is None else len(args.defaults)
+    if len(args.args) >= 1 and (len(args.args) - defaults <= 1):
+        return True
+    else:
+        return False
+
+
+def run_benchmark(im, module_list=[color, exposure, feature, filters, measure,
+                                   morphology, restoration, segmentation,
+                                   transform, util], skip_functions=[]):
+    times = {}
+
+    functions = []
+    for submodule in [exposure, feature, filters, measure, morphology,
+                                restoration, segmentation, transform, util]:
+        functions += inspect.getmembers(submodule, inspect.isfunction)
+
+
+    for function in functions:
+        args = inspect.getargspec(function[1])
+        only_one_argument = only_one_nondefault(args)
+        if function[0] in skip_functions:
+            continue
+        if only_one_argument:
+            try:
+                print(function[0])
+                start = default_timer()
+                function[1](im)
+                end = default_timer()
+                times[function[0]] = (end - start)
+            except TypeError:
+                print('wrong type ', function[0])
+            except:
+                print('error ', function[0])
+    return times
+
+
+
+im_uint8 = data.binary_blobs(length=1000, volume_fraction=0.3).astype(np.uint8)
+im_float = im_uint8.astype(np.float)
+
+skip_functions = ['hough_ellipse']
+times = run_benchmark(im_uint8, skip_functions=skip_functions)
+function_names = sorted(times, key=times.get)
+sorted_times = sorted(times.values())
+
+for func_name, t in zip(function_names, sorted_times):
+    print(func_name, t)
+
+skip_functions += function_names[-10:]
+bigim_uint8 = data.binary_blobs(length=4000,
+                    volume_fraction=0.3).astype(np.uint8)
+times = run_benchmark(bigim_uint8, skip_functions=skip_functions)
+function_names = sorted(times, key=times.get)
+sorted_times = sorted(times.values())
+
+for func_name, t in zip(function_names, sorted_times):
+    print(func_name, t)
+
+
